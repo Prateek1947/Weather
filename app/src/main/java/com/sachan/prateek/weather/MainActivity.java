d395a182970acc2f32167a748a32f72dd6c4b441
Prateek Sachan <psachan@cs.iitr.ac.in>
2019-04-06 22:47:32 +0530

    First Commit

---
+++ app/src/main/java/com/sachan/prateek/weather/MainActivity.java
@@ -0,0 +1,94 @@
+package com.sachan.prateek.weather;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.sachan.prateek.weather.Data.WeatherData;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String APIKEY = "f308b2139ca9071215ec0fd93173a019";
+    private Weather_Service weatherService;
+    private TextView city,lat,lon,country,temp,press,type,desc,humi,visibility,windSpeed,sunrise,sunset;
+    private ImageView imageView;
+    private static WeatherData weatherData;
+    private EditText editText;
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        weatherService = Retrofit_Builder.getRetrofit().create(Weather_Service.class);
+        instanstiate();
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fetchData(editText.getText().toString());
+                editText.selectAll();
+                editText.clearFocus();
+            }
+        });
+    }
+
+    void fetchData(String zipCode) {
+        weatherService.getWeatherData(APIKEY, zipCode + ",in").enqueue(new Callback<WeatherData>() {
+            @Override
+            public void onResponse(@NonNull Call<WeatherData> call, @NonNull Response<WeatherData> response) {
+                if (response.body() != null) {
+                    weatherData= response.body();
+                    city.setText(weatherData.getName());
+                    lon.setText(String.valueOf(weatherData.getCoord().getLon()));
+                    lat.setText(String.valueOf(weatherData.getCoord().getLat()));
+                    temp.setText(String.valueOf(weatherData.getMain().getTemp()));
+                    press.setText(String.valueOf(weatherData.getMain().getPressure()));
+                    humi.setText(String.valueOf(weatherData.getMain().getHumidity()));
+                    visibility.setText(String.valueOf(weatherData.getVisibility()));
+                    type.setText(weatherData.getWeather().get(0).getMain());
+                    desc.setText(weatherData.getWeather().get(0).getDescription());
+                    country.setText(weatherData.getSys().getCountry());
+                    windSpeed.setText(String.valueOf(weatherData.getWind().getSpeed()));
+                    Glide.with(MainActivity.this).load("http://openweathermap.org/img/w/" + response.body().getWeather().get(0).getIcon() + ".png").into(imageView);
+                } else {
+                    Toast.makeText(MainActivity.this, "Wrong Postal/Zip code", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onFailure(@NonNull Call<WeatherData> call, @NonNull Throwable t) {
+                t.printStackTrace();
+            }
+        });
+    }
+    private void instanstiate(){
+        imageView = findViewById(R.id.image);
+        city = findViewById(R.id.text);
+        lat=findViewById(R.id.lat);
+        country=findViewById(R.id.country);
+        lon=findViewById(R.id.lon);
+        temp=findViewById(R.id.temp);
+        humi=findViewById(R.id.hum);
+        press=findViewById(R.id.press);
+        type=findViewById(R.id.desc);
+        desc=findViewById(R.id.weather);
+        visibility=findViewById(R.id.vis);
+        windSpeed=findViewById(R.id.wind);
+        sunrise=findViewById(R.id.sunrise);
+        sunset=findViewById(R.id.sunset);
+        editText = findViewById(R.id.edit);
+        button = findViewById(R.id.bt);
+    }
+
+}
